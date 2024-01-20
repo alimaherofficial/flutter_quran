7c2774ce72642a9683607d461e5da5043040b19b
Ali Maher <ali840085@gmail.com>
2024-01-20 04:25:10 +0200

    Refactor QuranData class to improve code readability

--- lib/src/data/quran_data.dart
+++ lib/src/data/quran_data.dart
@@ -2,12 +2,12 @@
 /// It contains the surah number, ayah number ... etc
 /// of the Quran
 class QuranData {
-  
+
   /// [quranMap] is a map that contains the mapping of the Quran
   /// 
   /// It based on Quran
   /// 
-  /// key: is separated by a (:)
+  /// key: is separated by a (:) 
   /// 
   /// the first part is the surah number
   /// 
@@ -23,7 +23,9 @@ class QuranData {
   /// value: is a list of objects of surah number, start ayah number
   ///  and end ayah number
   ///
-  ///  example: [{"surah": 1, "start": 1, "end": 7}, ...}]
+  ///  example: ```dart
+  /// {"surah": 1, "start": 1, "end": 7},}
+  /// ```
   Map<String, dynamic> pageMap = {};
 
   /// [juzMap] is a map that contains the mapping of the juz
@@ -35,36 +37,22 @@ class QuranData {
   /// values of the sub-map: [id, surahs, verses]
   ///
   /// example:
-  ///
+  ///```dart
   /// '1': {
-  ///
   ///     'id': 1,
-  ///
   ///     'surahs': [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  ///
   ///     'verses': {
-  ///
   ///       '1': {'start': 1, 'end': 7},
-  ///
   ///       '2': {'start': 1, 'end': 141},
-  ///
   ///       '3': {'start': 1, 'end': 92},
-  ///
   ///       '4': {'start': 24, 'end': 147},
-  ///
   ///       '5': {'start': 82, 'end': 176},
-  ///
   ///       '6': {'start': 111, 'end': 165},
-  ///
   ///       '7': {'start': 1, 'end': 87},
-  ///
   ///       '8': {'start': 41, 'end': 75},
-  ///
   ///       '9': {'start': 93, 'end': 129},
-  ///
   ///     },
-  ///
-  ///   },
+  ///   },```
   Map<String, dynamic> juzMap = {};
 
   /// [sajdaMap] is a map that contains the mapping of the sajda
