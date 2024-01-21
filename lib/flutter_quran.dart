86bd2be12250e0cc9c51db6df74411d568887f71
Ali Maher <ali.maher@shopxco.com>
2024-01-22 01:30:32 +0200

    Update exports in flutter_quran.dart

--- lib/flutter_quran.dart
+++ lib/flutter_quran.dart
@@ -1,4 +1,11 @@
 // ignore: unnecessary_library_directive
 library flutter_quran;
 
-export 'src/quran.dart';
+export 'src/controller/quran.dart';
+export 'src/data/quran_data.dart';
+export 'src/models/aya_model/aya_model.dart';
+export 'src/models/recitations_model/recitations_model.dart';
+export 'src/models/sora_ayat_model/sora_ayat_model.dart';
+export 'src/models/sora_model/sora_model.dart';
+export 'src/models/sour_model/sour_model.dart';
+export 'src/view/quran_view.dart';
