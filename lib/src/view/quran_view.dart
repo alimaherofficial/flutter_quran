a35797ed04990aacb94b3205d805dc688d1eb4ff
Ali Maher <ali.maher@shopxco.com>
2024-01-22 01:31:09 +0200

    Add QuranView widget to lib/src/view/quran_view.dart

---
+++ lib/src/view/quran_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+/// [QuranView]
+class QuranView extends StatelessWidget {
+  /// [QuranView] constructor
+  const QuranView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
